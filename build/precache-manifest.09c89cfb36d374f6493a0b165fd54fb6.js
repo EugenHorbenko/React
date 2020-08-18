ec16376d04f449d2c822b48eedee041879565544
Eugen Horbenko <hogen3107@gmail.com>
2020-08-18 20:06:38 +0300

    added

---
+++ build/precache-manifest.09c89cfb36d374f6493a0b165fd54fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfc378f21179899026be0a925a4c3cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47c87e676b5e59dbeb43",
+    "url": "/static/css/main.a45a8319.chunk.css"
+  },
+  {
+    "revision": "29937a567330d3587a38",
+    "url": "/static/js/2.0483c0cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0483c0cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c87e676b5e59dbeb43",
+    "url": "/static/js/main.2c687be3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
